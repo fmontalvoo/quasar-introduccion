9aab0a3ca8469bf5e271810d63ea26893c9be00d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-12-29 18:07:34 -0500

    Manejo de filas y columnas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/pages/FlexPage.vue
+++ src/pages/FlexPage.vue
@@ -1,6 +1,39 @@
 <template>
-  <q-page class="flex">
-    <h2>FlexPage</h2>
+  <q-page>
+
+    <div class="row q-mb-sm">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-sm">
+      <div class="col-5 bg-primary"></div>
+      <div class="col-6 bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-sm justify-center">
+      <div class="col-4 bg-primary"></div>
+      <div class="col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-sm justify-between">
+      <div class="col-4 bg-primary"></div>
+      <div class="col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-sm justify-center">
+      <div class="col-4 bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-sm justify-between">
+      <div class="col-12 col-sm-4 col-md-6 bg-primary"></div>
+      <div class="col-12 col-sm-4 col-md-6 bg-accent"></div>
+    </div>
+
   </q-page>
 </template>
 
@@ -11,3 +44,12 @@ export default defineComponent({
   name: 'FlexPage'
 })
 </script>
+
+<style scoped>
+.col,
+.col-4,
+.col-6,
+.col-12 {
+  height: 50px;
+}
+</style>

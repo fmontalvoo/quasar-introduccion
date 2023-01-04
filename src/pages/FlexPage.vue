af68a7b4526726e11e53158455e9b27c2acc0ffa
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-01-04 11:58:27 -0500

    Flexbox
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/pages/FlexPage.vue
+++ src/pages/FlexPage.vue
@@ -34,6 +34,86 @@
       <div class="col-12 col-sm-4 col-md-6 bg-accent"></div>
     </div>
 
+    <q-separator spaced />
+
+    <div class="row">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row justify-between">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row justify-around">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row justify-end">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column items-start bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column items-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column items-end bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column custom-height items-center justify-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column custom-height items-center justify-between bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="column custom-height items-center justify-between bg-dark">
+      <div class="square-box self-start bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box self-end bg-accent"></div>
+    </div>
+
   </q-page>
 </template>
 
@@ -51,5 +131,14 @@ export default defineComponent({
 .col-6,
 .col-12 {
   height: 50px;
+}
+
+.square-box {
+  width: 50px;
+  height: 50px;
+}
+
+.custom-height {
+  height: 300px;
 }
 </style>
